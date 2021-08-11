5b51f0a9beb21c9defe7263ceb625a188359209d
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-08-11 13:14:13 -0400

    Update GenericRepository.cs

--- Data/Repositories/GenericRepository.cs
+++ Data/Repositories/GenericRepository.cs
@@ -4,41 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+#nullable enable
+
 namespace Sistema_facturacion.Data.Repositories
 {
     public abstract class GenericRepository<T> where T : class
     {
         public DBFacturacionContext Context { get; set; }
 
-        public GenericRepository(DBFacturacionContext context)
-        {
-            Context = context;
-        }
+        public GenericRepository(DBFacturacionContext context) => Context = context;
 
         public async Task<int> CreateAsync(T obj)
         {
-            Context.Set<T>().Add(obj);
+            await Context.Set<T>().AddAsync(obj);
             return await Context.SaveChangesAsync();
         }
 
-        public async Task<int> Update(T obj)
+        public async Task<int> UpdateAsync(T obj)
         {
             Context.Set<T>().Update(obj);
             return await Context.SaveChangesAsync();
         }
 
-        public async Task<int> Delete(T obj) { 
-
-            Context.Set<T>().Update(obj);
-            return await Context.SaveChangesAsync();
-        }
-
-        public async Task<int> GetSingle(T obj) { 
+        public async Task<int> DeleteAsync(T obj) { 
 
             Context.Set<T>().Remove(obj);
             return await Context.SaveChangesAsync();
         }
 
-        public async Task<List<T>> GetAll() => await Context.Set<T>().ToListAsync();
+        public async Task<T?> FindAsync(T obj) => await Context.Set<T>().FindAsync(obj);
+
+        public async Task<List<T>> GetAllAsync() => await Context.Set<T>().ToListAsync();
     }
 }
