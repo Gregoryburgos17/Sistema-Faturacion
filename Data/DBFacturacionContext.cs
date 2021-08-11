4dc10dc5cfcc5bdc8ee3682a919de02f9b1db671
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-08-11 12:30:23 -0400

    Add model classes and context

---
+++ Data/DBFacturacionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema_facturacion.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_facturacion.Data
+{
+    public class DBFacturacionContext : DbContext
+    {
+        public DBFacturacionContext(DbContextOptions<DBFacturacionContext> options)
+    : base(options)
+        {
+        }
+
+        public virtual DbSet<Producto> Productos { get; set; }
+    }
+}
